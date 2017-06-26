0709599578387308c672bfee217b27f9f3a9abeb
alvpia <alpia.memo@gmail.com>
2017-06-26 17:17:13 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,6 +1,18 @@
-var li = document.createElement('li');
+
 li.textContent
 
+for(var i=1;i<=100;1++){
+ var li = document.createElement('li');
+ if(i%15==0){
+  
+   li.textContent="FizzBuzz";
+   
+ }
+  
+  
+  document.getElementById('list').appendChild(li);
+  
+}
 
 document.getElementById('form').onsubmit = function(){
 window.alert(document.getElementById('form').word.value);
